c01cc8d8e201f9bc2bf4c062e5a37ce0a37abb2a
German Santos <santos.german@gmail.com>
2023-05-28 08:43:07 -0700

    Acting On Events lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+});
+
+console.log("The DOM has not loaded");
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+    //console.log(event);
+  });
+
